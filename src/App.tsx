990ad00582dacaa9e211abebcf9aa0ebd57e0d6b
ahmedibar <ahmednurali75@gmail.com>
2023-10-09 11:25:00 +0530

    Implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
         lg: `"nav nav" "aside main"`, // large devices or greater than 1024px
       }}
     >
-      <GridItem area="nav" bg="coral">
+      <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
