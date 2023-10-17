6784db60719bacaff127259d40fa81e641d68195
ahmedibar <ahmednurali75@gmail.com>
2023-10-17 18:10:22 +0530

    Refacto - Create Lookup Hoosk

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,21 +1,18 @@
 import { Heading } from "@chakra-ui/react";
 import { GameQuery } from "../App";
-import useGenres from "../hooks/useGenres";
+import useGenre from "../hooks/useGenre";
+import usePlatform from "../hooks/usePlatform";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  const { data: genres } = useGenres();
-  const genre = genres?.results.find((g) => g.id === gameQuery.genreId);
+  const genre = useGenre(gameQuery.genreId);
 
-  const { data: platforms } = useGenres();
-  const platform = platforms?.results.find((p) => p.id === gameQuery.platformId);
+  const platform = usePlatform(gameQuery.platformId);
 
-  const heading = `${platform?.name || ""} ${
-    genre?.name || ""
-  } Games`;
+  const heading = `${platform?.name || ""} ${genre?.name || ""} Games`;
 
   return (
     <Heading as="h1" marginY={5} fontSize="5xl">
